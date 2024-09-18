c9dfd6bec6401b5c13272cd911b44a555790ccb1
DESKTOP-VLJSSSG\armin <armin_donjivakuf@hotmail.com>
2024-09-18 16:39:19 +0200

    Added LogIn page as default startup page!

---
+++ northwind-mvc/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace northwind_mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("LogIn");
+        }
+    }
+}
